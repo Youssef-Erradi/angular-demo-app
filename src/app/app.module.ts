ea86dd040cdb0bf8b03f2b9829934e3de86ade43
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-03 16:55:07 +0100

    configure routing and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+const APP_ROUTES : Routes = [
+  {
+    path:'',
+    component : UsersComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'users',
+    component : UsersComponent
+  },
+  {
+    path:'user-form',
+    component : UserFormComponent
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    UsersComponent,
+    UserFormComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
