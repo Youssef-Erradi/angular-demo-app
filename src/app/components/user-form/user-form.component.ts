36949f359ff5d20ecef50cb8b665af257e9783b6
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-04 14:34:56 +0100

    send submitted user data to api

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  user:UserViewModel = {
+    name : '',
+    birthDate:'',
+    email:''
+  }
+  private url:string = 'http://localhost:8080/api/users'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  sendUser():void{
+    alert(this.user.name + '\n' +this.user.email+'\n'+this.user.birthDate)
+    this.http.post(this.url, this.user).subscribe(
+      { next : (v) => {location.reload()},
+        error : (e) => {alert(e.getMessage())}
+      }
+    )
+  }
+}
+
+export interface UserViewModel{
+  name:string
+  birthDate:string
+  email:String 
 }
