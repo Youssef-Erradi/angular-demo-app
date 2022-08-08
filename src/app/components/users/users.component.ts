b602e2fc3c7d03d1c3fcd25cc6a8d5dcb5760bfc
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-09 00:02:34 +0100

    add seach by keyword function

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 import { ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -9,11 +10,15 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  protected users!:Array<User>
+  users! : User[]
+  searchFormGroup! : FormGroup
 
-  constructor(private service:UserService, private route:ActivatedRoute) { }
+  constructor(private service:UserService, private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.searchFormGroup = this.formBuilder.group({
+      q : this.formBuilder.control("")
+    })
     this.getUsers()
   }
 
@@ -31,12 +36,17 @@ export class UsersComponent implements OnInit {
     if(confirm(`Are you sure you want to delete the user with ID ${user.id} ?`))
       this.service.delete(user.id!).subscribe(
         { next : () => {
-            this.users.splice(this.users.indexOf(user), 1)
+            delete this.users[this.users.indexOf(user)]
             alert("User deleted")
           },
           error : (e) => console.error(e)
         }
       )
+  }
+
+  protected searchByKeyword(){
+    const q = this.searchFormGroup.value.q.toLowerCase();
+    this.users = this.users.filter(user => user.name.toLowerCase().includes(q))
   }
 
 }
