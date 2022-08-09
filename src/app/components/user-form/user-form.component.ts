6a5b1bfff093250cfa7325c5b72c0f57824cdcf0
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-09 09:16:21 +0100

    use reactive forms

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
@@ -15,17 +16,22 @@ export class UserFormComponent implements OnInit {
     email:''
   }
 
-  constructor(private service:UserService, private route:ActivatedRoute, private router:Router) { }
+  protected formGroup!:FormGroup
+
+  constructor(private service:UserService, private route:ActivatedRoute, private router:Router,
+              private formBuiler:FormBuilder) { }
 
   ngOnInit(): void {
+    this.initFormGroup()
     let id:number = this.route.snapshot.params["id"]
     if(id)
-      this.getUser(id)
+      this.getUser(id)   
   }
 
   saveUser():void{
+    this.user = this.formGroup.value
     this.service.save(this.user).subscribe(
-      { next : (n) => this.router.navigate(['/users']),
+      { next : () => this.router.navigate(['/users']),
         error : (e) => console.error(e)
       }
     )
@@ -33,13 +39,36 @@ export class UserFormComponent implements OnInit {
 
   getUser(id:number):void{
     this.service.get(id).subscribe(
-      { next : (n) => {
-        this.user = n
-        this.user["id"] = n["id"]
+      { next : (user) => {
+        this.user = user
+        this.initFormGroup()
       },
         error : (e) => console.error(e)
       }
     )
+  }
+
+  getErrorMessage(fieldName:string) : string | void{
+    const fieldErrors = this.formGroup.controls[fieldName].errors
+    if(!fieldErrors) return
+
+    fieldName = fieldName.charAt(0).toUpperCase() + fieldName.slice(1)
+    let message = ''
+    if(fieldErrors['required'])
+      message += `${fieldName} is required\n`
+    if(fieldErrors['email'])
+      message += `The value entered is not a valid email address`
+
+    return message;
+  }
+
+  private initFormGroup(){
+    this.formGroup = this.formBuiler.group({
+      id: this.formBuiler.control(this.user.id),
+      name: this.formBuiler.control(this.user.name, [Validators.required]),
+      email: this.formBuiler.control(this.user.email, [Validators.required, Validators.email]),
+      birthDate: this.formBuiler.control(this.user.birthDate, [Validators.required]),
+    })
   }
 
 }
