cad4c7ce15ea0f587a43f5b7576d1e1a784a6dfc
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-12 04:06:55 +0100

    add reactive form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  protected formGroup! : FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      email : this.fb.control('', [Validators.required, Validators.email]),
+      password : this.fb.control('', [Validators.required,])
+    })
+  }
+
+  getErrorMessage(fieldName:string) : string | void{
+    const fieldErrors = this.formGroup.controls[fieldName].errors
+    if(!fieldErrors) return
+
+    fieldName = fieldName.charAt(0).toUpperCase() + fieldName.slice(1)
+    let message = ''
+    if(fieldErrors['required'])
+      message += `${fieldName} is required\n`
+    if(fieldErrors['email'])
+      message += `The value entered is not a valid email address`
+
+    return message;
+  }
+
+  login(){
+
   }
 
 }
