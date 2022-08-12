6c143f44c180bafbbf9e2e2e785afd7016eca553
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-12 04:33:36 +0100

    add login method to component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,32 +9,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  protected formGroup! : FormGroup;
-  constructor(private fb:FormBuilder) { }
+  protected formGroup!: FormGroup;
+
+  constructor(private fb: FormBuilder, private auth: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
     this.formGroup = this.fb.group({
-      email : this.fb.control('', [Validators.required, Validators.email]),
-      password : this.fb.control('', [Validators.required,])
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [Validators.required,])
     })
   }
 
-  getErrorMessage(fieldName:string) : string | void{
+  getErrorMessage(fieldName: string): string | void {
     const fieldErrors = this.formGroup.controls[fieldName].errors
-    if(!fieldErrors) return
+    if (!fieldErrors) return
 
     fieldName = fieldName.charAt(0).toUpperCase() + fieldName.slice(1)
     let message = ''
-    if(fieldErrors['required'])
+    if (fieldErrors['required'])
       message += `${fieldName} is required\n`
-    if(fieldErrors['email'])
+    if (fieldErrors['email'])
       message += `The value entered is not a valid email address`
 
     return message;
   }
 
-  login(){
-
+  login() {
+    const { email, password } = this.formGroup.value
+    this.auth.login(email, password).subscribe({
+      next: tokens => this.auth.storeTokens(tokens).subscribe({
+        next: () => this.router.navigateByUrl("/")
+      }),
+      error: e => alert("Bad Credentials")
+    })
   }
 
 }
